5c61504177776238f565e74dbe0f95f52141c402
Michael Hobbs <michael.lee.hobbs@gmail.com>
2025-04-18 10:23:04 -0400

    updated Code section to be an actual code block

--- src/projectInfo.ts
+++ src/projectInfo.ts
@@ -168,9 +168,12 @@ export function appendPromptInstructions(log: (msg: string) => void, projectInfo
 
   log(`\n
 ## Code:
+\`\`\`
 [Include code snippets or references]
+\`\`\`
 
 Instructions:
+- Please give the full updated code for any files that were changed in a Markdown code block.
 - Provide a summary of any issues found.
 - Suggest improvements with code examples if possible.
 
